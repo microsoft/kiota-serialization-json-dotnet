b5563ae62694871f786ac6673807f384ade56243
Vincent Biret <vibiret@microsoft.com>
2022-07-08 16:13:17 -0400

    - implements deserialization of composed types

---
+++ Microsoft.Kiota.Serialization.Json.Tests/Mocks/IntersectionTypeMock.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Kiota.Abstractions.Serialization;
+
+namespace Microsoft.Kiota.Serialization.Json.Tests.Mocks;
+
+public class IntersectionTypeMock : IIntersectionWrapper, IParsable
+{
+    public TestEntity ComposedType1 { get; set; }
+    public SecondTestEntity ComposedType2 { get; set; }
+    public string StringValue { get; set; }
+    public static IntersectionTypeMock CreateFromDiscriminator(IParseNode parseNode) {
+        var result = new IntersectionTypeMock();
+        var discriminator = parseNode.GetChildNode("@odata.type")?.GetStringValue();
+        if("#microsoft.graph.testEntity".Equals(discriminator)) {
+            result.ComposedType1 = new();
+            result.DeserializationHint = "ComposedType1";
+        }
+        else if("#microsoft.graph.secondTestEntity".Equals(discriminator)) {
+            result.ComposedType2 = new();
+            result.DeserializationHint = "ComposedType2";
+        }
+        else if (parseNode.GetStringValue() is string stringValue) {
+            result.StringValue = stringValue;
+            result.DeserializationHint = "kiota-deserialization-done";
+        }
+        return result;
+    }
+    public string DeserializationHint { get; set; }
+    public IDictionary<string, Action<IParseNode>> GetFieldDeserializers() => throw new NotImplementedException();
+    public void Serialize(ISerializationWriter writer) => throw new NotImplementedException();
+}
---
+++ Microsoft.Kiota.Serialization.Json.Tests/Mocks/UnionTypeMock.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Kiota.Abstractions.Serialization;
+
+namespace Microsoft.Kiota.Serialization.Json.Tests.Mocks;
+
+public class UnionTypeMock : IUnionWrapper, IParsable
+{
+    public TestEntity ComposedType1 { get; set; }
+    public SecondTestEntity ComposedType2 { get; set; }
+    public string StringValue { get; set; }
+    public static UnionTypeMock CreateFromDiscriminator(IParseNode parseNode) {
+        var result = new UnionTypeMock();
+        result.ComposedType1 = new();
+        result.ComposedType2 = new();
+        result.DeserializationHint = "ComposedType1;ComposedType2;";
+        if (parseNode.GetStringValue() is string stringValue) {
+            result.StringValue = stringValue;
+            result.DeserializationHint = "kiota-deserialization-done";
+        }
+        return result;
+    }
+    public string DeserializationHint { get; set; }
+    public IDictionary<string, Action<IParseNode>> GetFieldDeserializers() => throw new NotImplementedException();
+    public void Serialize(ISerializationWriter writer) => throw new NotImplementedException();
+}
