79622333b5241f1202db1707ef196ab7c0200d1a
Vincent Biret <vibiret@microsoft.com>
2022-07-11 11:26:36 -0400

    - resolves confusion between union and intersection types
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- Microsoft.Kiota.Serialization.Json.Tests/IntersectionWrapperParseTests.cs
+++ Microsoft.Kiota.Serialization.Json.Tests/IntersectionWrapperParseTests.cs
@@ -7,12 +7,13 @@ namespace Microsoft.Kiota.Serialization.Json.Tests;
 
 public class IntersectionWrapperParseTests {
     private readonly JsonParseNodeFactory _parseNodeFactory = new();
+    private readonly JsonSerializationWriterFactory _serializationWriterFactory = new();
     private const string contentType = "application/json";
     [Fact]
     public void ParsesIntersectionTypeComplexProperty1()
     {
         // Given
-        using var payload = new MemoryStream(Encoding.UTF8.GetBytes("{\"@odata.type\":\"#microsoft.graph.testEntity\",\"officeLocation\":\"Montreal\", \"id\": \"opaque\"}"));
+        using var payload = new MemoryStream(Encoding.UTF8.GetBytes("{\"displayName\":\"McGill\",\"officeLocation\":\"Montreal\", \"id\": \"opaque\"}"));
         var parseNode = _parseNodeFactory.GetRootParseNode(contentType, payload);
     
         // When
@@ -21,18 +22,17 @@ public class IntersectionWrapperParseTests {
         // Then
         Assert.NotNull(result);
         Assert.NotNull(result.ComposedType1);
-        Assert.Null(result.ComposedType2);
+        Assert.NotNull(result.ComposedType2);
         Assert.Null(result.StringValue);
-        Assert.StartsWith("ComposedType1", result.DeserializationHint);
-        Assert.DoesNotContain("ComposedType2", result.DeserializationHint);
-        Assert.DoesNotContain("kiota-deserialization-done", result.DeserializationHint);
+        Assert.Equal("ComposedType1;ComposedType2;", result.DeserializationHint);
         Assert.Equal("opaque", result.ComposedType1.Id);
+        Assert.Equal("McGill", result.ComposedType2.DisplayName);
     }
     [Fact]
     public void ParsesIntersectionTypeComplexProperty2()
     {
         // Given
-        using var payload = new MemoryStream(Encoding.UTF8.GetBytes("{\"@odata.type\":\"#microsoft.graph.secondTestEntity\",\"officeLocation\":\"Montreal\", \"id\": 10}"));
+        using var payload = new MemoryStream(Encoding.UTF8.GetBytes("{\"displayName\":\"McGill\",\"officeLocation\":\"Montreal\", \"id\": 10}"));
         var parseNode = _parseNodeFactory.GetRootParseNode(contentType, payload);
     
         // When
@@ -40,13 +40,13 @@ public class IntersectionWrapperParseTests {
     
         // Then
         Assert.NotNull(result);
+        Assert.NotNull(result.ComposedType1);
         Assert.NotNull(result.ComposedType2);
-        Assert.Null(result.ComposedType1);
         Assert.Null(result.StringValue);
-        Assert.StartsWith("ComposedType2", result.DeserializationHint);
-        Assert.DoesNotContain("ComposedType1", result.DeserializationHint);
-        Assert.DoesNotContain("kiota-deserialization-done", result.DeserializationHint);
-        Assert.Equal(10, result.ComposedType2.Id);
+        Assert.Equal("ComposedType1;ComposedType2;", result.DeserializationHint);
+        Assert.Null(result.ComposedType1.Id);
+        Assert.Null(result.ComposedType2.Id); // it's expected to be null since we have conflicting properties here and the parser will only try one to avoid having to brute its way through
+        Assert.Equal("McGill", result.ComposedType2.DisplayName);
     }
     [Fact]
     public void ParsesIntersectionTypeStringValue()
@@ -60,11 +60,74 @@ public class IntersectionWrapperParseTests {
     
         // Then
         Assert.NotNull(result);
-        Assert.Null(result.ComposedType2);
-        Assert.Null(result.ComposedType1);
+        Assert.NotNull(result.ComposedType2);
+        Assert.NotNull(result.ComposedType1);
         Assert.Equal("officeLocation", result.StringValue);
         Assert.DoesNotContain("ComposedType1", result.DeserializationHint);
         Assert.DoesNotContain("ComposedType2", result.DeserializationHint);
         Assert.StartsWith("kiota-deserialization-done", result.DeserializationHint);
+    }
+    [Fact]
+    public void SerializesIntersectionTypeStringValue()
+    {
+        // Given
+        using var writer = _serializationWriterFactory.GetSerializationWriter(contentType);
+        var model = new IntersectionTypeMock {
+            StringValue = "officeLocation"
+        };
+    
+        // When
+        model.Serialize(writer);
+        using var resultStream = writer.GetSerializedContent();
+        using var streamReader = new StreamReader(resultStream);
+        var result = streamReader.ReadToEnd();
+    
+        // Then
+        Assert.Equal("\"officeLocation\"", result);
+    }
+    [Fact]
+    public void SerializesIntersectionTypeComplexProperty1()
+    {
+        // Given
+        using var writer = _serializationWriterFactory.GetSerializationWriter(contentType);
+        var model = new IntersectionTypeMock {
+            ComposedType1 = new() {
+                Id = "opaque",
+                OfficeLocation = "Montreal",
+            },
+            ComposedType2 = new() {
+                DisplayName = "McGill",
+            },
+        };
+    
+        // When
+        model.Serialize(writer);
+        using var resultStream = writer.GetSerializedContent();
+        using var streamReader = new StreamReader(resultStream);
+        var result = streamReader.ReadToEnd();
+    
+        // Then
+        Assert.Equal("{\"id\":\"opaque\",\"officeLocation\":\"Montreal\"}", result);
+    }
+    [Fact]
+    public void SerializesIntersectionTypeComplexProperty2()
+    {
+        // Given
+        using var writer = _serializationWriterFactory.GetSerializationWriter(contentType);
+        var model = new IntersectionTypeMock {
+            ComposedType2 = new() {
+                DisplayName = "McGill",
+                Id = 10,
+            },
+        };
+    
+        // When
+        model.Serialize(writer);
+        using var resultStream = writer.GetSerializedContent();
+        using var streamReader = new StreamReader(resultStream);
+        var result = streamReader.ReadToEnd();
+    
+        // Then
+        Assert.Equal("{\"displayName\":\"McGill\",\"id\":10}", result);
     }
 }
